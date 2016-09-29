bade5e09ea46c3740bef7ac535cbb7b206fc0260
Valia Fetisov <valyalof@gmail.com>
2016-09-29 18:02:35 +0200

    dochead added to support viewport metatag

--- imports/layouts/RootLayout.jsx
+++ imports/layouts/RootLayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { DocHead } from 'meteor/kadira:dochead'
 
 const RootLayout = React.createClass({
 
   render () {
+    DocHead.addMeta({name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1'})
+
     return <div className="RootLayout">
       {this.props.content}
     </div>
