4d4a43f9171b6ae72e687b4a250203c81e1234ff
Valia Fetisov <valyalof@gmail.com>
2016-09-30 02:27:20 +0200

    favicons added

--- imports/layouts/RootLayout.jsx
+++ imports/layouts/RootLayout.jsx
@@ -5,6 +5,14 @@ const RootLayout = React.createClass({
 
   render () {
     DocHead.addMeta({name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1'})
+    DocHead.addLink({rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png'})
+    DocHead.addLink({rel: 'icon', type: 'image/png', href: '/favicons/favicon-32x32.png', sizes: '32x32'})
+    DocHead.addLink({rel: 'icon', type: 'image/png', href: '/favicons/favicon-16x16.png', sizes: '16x16'})
+    DocHead.addLink({rel: 'manifest', href: '/favicons/manifest.json'})
+    DocHead.addLink({rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#171717'})
+    DocHead.addLink({rel: 'shortcut icon', href: '/favicon.ico'})
+    DocHead.addMeta({name: 'msapplication-config', content: '/favicons/browserconfig.xml'})
+    DocHead.addMeta({name: 'theme-color', content: '#ffffff'})
 
     return <div className="RootLayout">
       {this.props.content}
