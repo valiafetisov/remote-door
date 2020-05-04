179f4e029d2e4b4854c1556d7b5304ef07c00b07
Valia Fetisov <valyalof@gmail.com>
2020-05-05 01:07:00 +0200

    send mqtt messages from the client

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
+const url = require('url')
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const openTheDoor = require('./mqtt')
 
 const PORT = process.env.PORT || 7002
 const app = express()
@@ -12,8 +14,12 @@ server.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
 app.use(express.static('dist'))
 
 io.on('connection', (socket) => {
-  socket.send({ hello: 'world' })
-  socket.on('message', (data) => {
-    console.log('message', data)
+  // socket.send({ hello: 'world' })
+  socket.on('message', (message) => {
+    const referer = url.parse(socket.handshake.headers.referer)
+    const { hostname } = referer
+    if (message === 'open') {
+      openTheDoor(hostname)
+    }
   })
 })
