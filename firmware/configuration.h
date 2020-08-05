c1445aa230a52f6735adc67dc09e966aa8a86a25
valia fetisov <contact@valiafetisov.com>
2020-08-05 15:33:41 +0200

    delete invalidated configuration.h

--- firmware/configuration.h
+++
@@ -1,6 +0,0 @@
-const char* wifiSSID = "FRITZ!Box Fon WLAN 7360";
-const char* wifiPassword = "54963444978096584175";
-const char* mqttServer = "192.168.178.52";
-const char* mqttClientName = "ESP8266_in_Cologne";
-const char* mqttUser = "";
-const char* mqttPassword = "";
